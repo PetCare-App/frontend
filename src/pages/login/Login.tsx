6a28a86f374dbb7727c64c638d18985b478da1a4
pedropeglow <pedropeglowm@gmail.com>
2023-04-25 11:31:59 -0300

    :lipstick: Estilizando login.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,28 @@
 import { Button, Input } from "@mui/material";
 import { useForm } from "react-hook-form";
-import { Container, ErrorSpan, Form, Header } from "./styles";
+import {
+  Container,
+  Content,
+  ErrorSpan,
+  Form,
+  Header,
+  LabelSignup,
+  Strong,
+} from "./styles";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from 'yup';
+import * as yup from "yup";
 import { login } from "../../services/loginService";
+import { Link } from "react-router-dom";
 
 const schema = yup.object().shape({
-  email: yup.string().required("Campo e-mail obrigatório").email("Digite um e-mail válido!"),
-  password: yup.string().required("Campo senha é obrigatório").min(8, "Senha deve conter no mínimo 8 dígitos")
+  email: yup
+    .string()
+    .required("Campo e-mail obrigatório")
+    .email("Digite um e-mail válido!"),
+  password: yup
+    .string()
+    .required("Campo senha é obrigatório")
+    .min(8, "Senha deve conter no mínimo 8 dígitos"),
 });
 
 interface FormData {
@@ -19,32 +34,45 @@ export const Login = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid }
+    formState: { errors, isValid },
   } = useForm<FormData>({
     mode: "onChange",
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema),
   });
   const onSubmit = async (data: FormData) => {
     try {
       await login(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   return (
     <Container>
       <Header className="header">
         <span>Por favor digite suas informações de login</span>
       </Header>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Input placeholder="E-mail" {...register("email")} />
-        {errors.email && <ErrorSpan>{errors.email.message}</ErrorSpan>}
-        <Input placeholder="Senha" type="password" {...register("password", { required: true })} />
-        {errors.password && <ErrorSpan>{errors.password.message}</ErrorSpan>}
-        <Button disabled={!isValid} variant="contained" type="submit">Login</Button>
-      </Form>
+      <Content>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Input placeholder="E-mail" {...register("email")} />
+          {errors.email && <ErrorSpan>{errors.email.message}</ErrorSpan>}
+          <Input
+            placeholder="Senha"
+            type="password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && <ErrorSpan>{errors.password.message}</ErrorSpan>}
+          <Button disabled={!isValid} variant="contained" type="submit">
+            Login
+          </Button>
+          <LabelSignup>
+            Não tem uma conta?
+            <Strong>
+              <Link to="/register">&nbsp;Registre-se</Link>
+            </Strong>
+          </LabelSignup>
+        </Form>
+      </Content>
     </Container>
-  )
+  );
 };
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100vw;
@@ -8,7 +8,21 @@ export const Container = styled.div`
   justify-content: center;
   flex-direction: column;
   background-color: white;
-  `;
+`;
+
+export const Content = styled.div`
+  gap: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0 1px 2px #410b13;
+  background-color: white;
+  max-width: 350px;
+  padding: 20px;
+  border-radius: 5px;
+`;
 
 export const Header = styled.header`
   display: flex;
@@ -17,7 +31,7 @@ export const Header = styled.header`
   margin-bottom: 40px;
   color: black;
 
-  span{
+  span {
     font-weight: 500;
     font-size: 20px;
     line-height: 24px;
@@ -31,7 +45,7 @@ export const Form = styled.form`
   flex-direction: column;
   row-gap: 8px;
 
-  input{
+  input {
     display: flex;
     flex-direction: row;
     align-items: flex-start;
@@ -39,17 +53,28 @@ export const Form = styled.form`
     width: 250px;
     height: 20px;
     border-radius: 12px;
-    &:focus{
+    &:focus {
       border-color: black;
     }
-    &:hover{
+    &:hover {
       background-color: aquamarine;
     }
-
   }
+`;
 
-  `;
-
-export const ErrorSpan = styled.span `
+export const ErrorSpan = styled.span`
   color: red;
-`
+`;
+
+export const LabelSignup = styled.label`
+  font-size: 16px;
+  color: #676767;
+`;
+
+export const Strong = styled.strong`
+  cursor: pointer;
+  a {
+    text-decoration: none;
+    color: #676767;
+  }
+`;
