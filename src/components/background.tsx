aaef058504e7283f442b794731579ba81fa8bf3f
nadine zingano <nadine.zingano@ifood.com.br>
2023-04-30 19:54:06 -0300

    background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,50 +1,72 @@
 import styled from '@emotion/styled';
-import { Box, Container, Grid, Paper, css, useTheme } from '@mui/material';
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  ThemeOptions,
+  css,
+  useTheme,
+} from '@mui/material';
 
-const Background = ({ children }: { children: JSX.Element | any }) => {
-  const theme = useTheme();
+interface Props {
+  color?: string;
+  theme: any;
+}
 
-  const Wrapper = styled(Container)`
+const Circle = styled(Box)<Props>`
+  ${({ color, theme }) => css`
+    background-color: ${color === 'orange' ? '#feb06a' : '#36d6e7'};
+    border-radius: 50%;
+    height: 20rem;
+    width: 20rem;
+    margin-left: ${color === 'blue' && 'calc(100vw - 360px)'};
+    ${theme.breakpoints.down('sm')} {
+      display: none;
+    }
+  `}
+`;
+
+const Wrapper = styled(Container)<Props>`
+  ${({ theme }) => css`
     background-color: #e4c9e5;
     height: 100vh;
     width: 100vw;
     display: grid;
     align-content: space-around;
     position: relative;
+    margin: 0;
+    padding: 0;
 
     ${theme.breakpoints.down('sm')} {
       align-content: space-between;
       padding-top: 20px;
       padding-bottom: 20px;
     }
-  `;
 
-  const Circle = styled(Box)<{ color: string }>`
-    background-color: ${(props) =>
-      props.color === 'orange' ? '#feb06a' : '#36d6e7'};
-    border-radius: 50%;
-    height: 20rem;
-    width: 20rem;
-    margin-left: ${(props) => props.color === 'blue' && 'calc(100vw - 360px)'};
-    ${theme.breakpoints.down('sm')} {
-      height: 10rem;
-      width: 10rem;
-      margin-left: ${(props) =>
-        props.color === 'blue' && 'calc(100vw - 190px)'};
+    ${theme.breakpoints.up('sm')} {
+      max-width: 100%;
     }
-  `;
+  `}
+`;
 
-  const Card = styled(Paper)`
+const Card = styled(Paper)<Props>`
+  ${({ theme }) => css`
     height: calc(100vh - 250px);
     width: calc(100vw - 200px);
+    max-width: 1200px;
     ${theme.breakpoints.down('sm')} {
-      width: calc(100vw - 100px);
-      height: calc(100vh - 100px);
+      width: calc(100vw - 50px);
+      height: calc(100vh - 50px);
     }
-  `;
+  `}
+`;
+
+const Background = ({ children }: { children: JSX.Element | any }) => {
+  const theme = useTheme();
 
   return (
-    <Wrapper>
+    <Wrapper theme={theme}>
       <Box
         sx={{
           position: 'absolute',
@@ -56,10 +78,12 @@ const Background = ({ children }: { children: JSX.Element | any }) => {
           justifyContent: 'center',
         }}
       >
-        <Card elevation={0}>{children}</Card>
+        <Card elevation={0} theme={theme}>
+          {children}
+        </Card>
       </Box>
-      <Circle color={'orange'} />
-      <Circle color={'blue'} />
+      <Circle color={'orange'} theme={theme} />
+      <Circle color={'blue'} theme={theme} />
     </Wrapper>
   );
 };
---
+++ src/components/menu/types.ts
@@ -0,0 +1,4 @@
+export interface MenuProps {
+  content: number;
+  setContent: React.Dispatch<React.SetStateAction<number>>;
+}
