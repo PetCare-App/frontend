6b2d8dffb35e8ecc2e851367a8d9e3a16b884bb3
Nadine Zingano <nadine.zingano@mitzit.com.br>
2023-05-24 16:49:33 -0300

    card

--- src/pages/pets/Dashboard.tsx
+++ src/pages/pets/Dashboard.tsx
@@ -1,11 +1,16 @@
 import React, {useEffect, useState} from 'react'
 import { usePetCareContext } from "../../context";
 
-import { Card, CardContent, Typography, CardActions, Button, Container, Box, IconButton } from '@mui/material'
+import { Card, CardContent, Typography, CardActions, Button, Container, Box, IconButton, Avatar, Stack } from '@mui/material'
 import { Pet } from '../../types/pets';
 import { useNavigate } from 'react-router-dom';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import Paw from './../../assets/paw.png'
+import Dog from './../../assets/dog.png'
+import Cat from './../../assets/cat.png'
 
 interface DashboardProps {
   handleOpenCreateForm: () => void
@@ -16,6 +21,15 @@ export const Dashboard = ({handleOpenCreateForm, handleOpenEditForm}: DashboardP
   const {pets, getPets} = usePetCareContext()
 
   useEffect(() => {getPets()}, [])
+
+  const dateFormat = (date: any ) => {
+    const deleteTimestamp = date?.split('T')[0]
+    const day = deleteTimestamp.split('-')[2]
+    const month = deleteTimestamp.split('-')[1]
+    const year = deleteTimestamp.split('-')[0]
+
+    return `${day}/${month}/${year}`
+  }
 
   return(
     <>
@@ -29,16 +43,36 @@ export const Dashboard = ({handleOpenCreateForm, handleOpenEditForm}: DashboardP
       !!pets.length && (
         pets.map((pet: Pet) => {
           return( 
-          <Card variant="outlined" key={pet?.id} sx={{height: '200px', width: '200px', marginBottom: '20px'}}> 
-            <CardContent >
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom >
-                {pet?.name}
-              </Typography>
+          <Card variant="outlined" key={pet?.id} sx={{height: '200px', width: '200px', marginBottom: '20px', padding: '10px'}}> 
+            <CardContent sx={{padding: '10px'}}>
+              <Stack direction="row" justifyContent="space-between" alignItems="center">
+                <Avatar src={pet?.animalType == 'Cat' ? Cat : pet?.animalType == 'Dog'? Dog : Paw} sx={{height: '50px', width: '50px'}}/>
+                <Typography sx={{ fontSize: 16, fontWeight: 600 }} color="text.primary" variant='h3' gutterBottom >
+                  {pet?.name}
+                </Typography>
+              </Stack>
+              <Stack direction="column" justifyContent="flex-end" alignItems="flex-start" height="100px">
+                <Typography sx={{ fontSize: 15 }} color="text.primary" >
+                  { `Raça: ${pet?.breed}`}
+                </Typography>
+                <Typography sx={{ fontSize: 15 }} color="text.primary" >
+                  { `Aniversário: ${dateFormat(pet?.birthDate)}`}
+                </Typography>
+                <Typography sx={{ fontSize: 15 }} color="text.primary" >
+                  { `Gênero: ${pet?.gender}`}
+                </Typography>
+                <Typography sx={{ fontSize: 15 }} color="text.primary" >
+                  { `Peso: ${pet?.weight} Kgs`}
+                </Typography>
+              </Stack>
+    
             </CardContent>
-            <CardActions >
-            <Button size="small" >Learn More</Button>
+            <CardActions sx={{display:'flex', flexDirection: 'row', justifyContent: 'space-evenly', padding: '0px'}}>
             <IconButton onClick={() => handleOpenEditForm(pet)}>
-                <EditIcon sx={{ fontSize: '20px'}}/>
+                <DeleteIcon sx={{ fontSize: '25px'}}/>
+              </IconButton>
+            <IconButton onClick={() => handleOpenEditForm(pet)}>
+                <EditIcon sx={{ fontSize: '25px'}}/>
               </IconButton>
             </CardActions>
           </Card>
