fca50d7f0e7844613808539bd58ab0e57169483e
Nadine Zingano <nadine.zingano@mitzit.com.br>
2023-05-24 15:20:38 -0300

    interfaces

---
+++ src/types/pets.ts
@@ -0,0 +1,10 @@
+export interface Pet {
+  id: string;
+  name: string;
+  breed: string;
+  animalType: string | null;
+  gender: string;
+  weight?: number | string;
+  birthDate?: string;
+  userId: number;
+}
