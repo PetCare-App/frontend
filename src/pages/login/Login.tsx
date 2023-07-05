f3d26be8031c4d795f74faf4eb09a110990da655
pedropeglow <pedropeglowm@gmail.com>
2023-07-05 17:54:13 -0300

    :bug: Ajuste nos paths.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -16,7 +16,7 @@ import { login } from "../../services/loginService";
 import { Link, useNavigate } from "react-router-dom";
 import Background from "../../components/background";
 import logoImg from "../../assets/logo.png";
-import {  useState } from "react";
+import { useState } from "react";
 import { usePetCareContext } from "../../context";
 
 const schema = yup.object().shape({
@@ -48,18 +48,20 @@ export const Login = () => {
     resolver: yupResolver(schema),
   });
 
-  const {setUser} = usePetCareContext()
+  const { setUser } = usePetCareContext();
 
   const onSubmit = async (data: FormData) => {
     try {
       await login(data).then((response) => {
-        const decodedToken = JSON.parse(atob(response.data.token.split('.')[1]));
+        const decodedToken = JSON.parse(
+          atob(response.data.token.split(".")[1])
+        );
         setUser({
-          email: decodedToken.email, 
+          email: decodedToken.email,
           id: decodedToken.sub,
           fullname: decodedToken.fullname,
-        })
-        navigate("/");
+        });
+        navigate("/pets");
       });
     } catch (error) {
       setError("Usuário ou senha incorretos.");
