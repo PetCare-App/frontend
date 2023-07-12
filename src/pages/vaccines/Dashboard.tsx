3718e8a4d0035f30d379da6e5c9e7c4bf1a30ca1
Nadine Zingano <nadine.zingano@mitzit.com.br>
2023-07-12 17:40:13 -0300

    card dashboard vacinas

--- src/pages/vaccines/Dashboard.tsx
+++ src/pages/vaccines/Dashboard.tsx
@@ -79,13 +79,86 @@ export const Dashboard = ({
 			>
 				{!!vaccines.length && !loading ? (
 					vaccines.map((vaccine: Vaccine) => {
-						const pet = pets.find((pet: Pet) => pet.id === vaccines.petId);
+						const pet = pets.find((pet: any) => pet.id === vaccine.petId);
 						return (
+							// <Card
+							// 	variant='outlined'
+							// 	key={vaccine?.id}
+							// 	sx={{
+							// 		height: '200px',
+							// 		width: '200px',
+							// 		marginBottom: '20px',
+							// 		padding: '10px',
+							// 	}}
+							// >
+							// 	<CardContent sx={{ padding: '10px' }}>
+							// 		<Stack
+							// 			direction='row'
+							// 			justifyContent='space-between'
+							// 			alignItems='center'
+							// 		>
+							// 			{pet && (
+							// 				<Typography
+							// 					sx={{ fontSize: 24, fontWeight: 600 }}
+							// 					color='text.secondary'
+							// 					variant='h3'
+							// 					gutterBottom
+							// 				>
+							// 					{pet?.name}
+							// 				</Typography>
+							// 			)}
+							// 		</Stack>
+							// 		<Stack
+							// 			direction='row'
+							// 			justifyContent='space-between'
+							// 			alignItems='center'
+							// 		>
+							// 			<Typography
+							// 				sx={{ fontSize: 16, fontWeight: 600 }}
+							// 				color='text.primary'
+							// 				variant='h3'
+							// 				gutterBottom
+							// 			>
+							// 				{vaccine?.name}
+							// 			</Typography>
+							// 		</Stack>
+							// 		<Stack
+							// 			direction='column'
+							// 			justifyContent='flex-end'
+							// 			alignItems='flex-start'
+							// 			height='35px'
+							// 		>
+							// 			<Typography
+							// 				sx={{ fontSize: 15 }}
+							// 				color='text.primary'
+							// 			>
+							// 				{`Data: ${dateFormat(vaccine.date)}`}
+							// 			</Typography>
+							// 		</Stack>
+							// 	</CardContent>
+							// 	<CardActions
+							// 		sx={{
+							// 			display: 'flex',
+							// 			flexDirection: 'row',
+							// 			justifyContent: 'space-evenly',
+							// 			padding: '0px',
+							// 		}}
+							// 	>
+							// 		<IconButton
+							// 			onClick={() => handleOpenDeleteConfirmation(vaccine)}
+							// 		>
+							// 			<DeleteIcon sx={{ fontSize: '25px' }} />
+							// 		</IconButton>
+							// 		<IconButton onClick={() => handleOpenEditForm(vaccine)}>
+							// 			<EditIcon sx={{ fontSize: '25px' }} />
+							// 		</IconButton>
+							// 	</CardActions>
+							// </Card>
 							<Card
 								variant='outlined'
 								key={vaccine?.id}
 								sx={{
-									height: '200px',
+									height: '150px',
 									width: '200px',
 									marginBottom: '20px',
 									padding: '10px',
